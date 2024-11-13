07d39bdb6d9e1c979b2b9d600396d112177e2e05
ducvu <ducvu973xd@gmail.com>
2024-11-13 13:37:29 +0700

    inital config

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+
+const App = () => {
+  return (
+    <Box>App</Box>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider value={defaultSystem}>
+          <App />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
